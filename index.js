8725bf3b0778c09a4694f94c862f7e0bd28b6fc1
Chad96 <chadrackndalamba@gmail.com>
2024-04-20 11:21:46 +0200

    index.js

--- index.js
+++ index.js
@@ -2,15 +2,12 @@
 const name = "Chadrack";
 const surname = "Ndalamba";
 const bio =
-  "Hello - my name is Chadrack Ndalamba. I am a young, professional, hardworking individual who is always motivated to show the best version of myself...";
+  "<b>Hello -</b> my name is Chadrack Ndalamba. I am a young, professional, hardworking individual who is always motivated to show the best version of myself.<br /><br />I am an aspiring full-stack software engineer and a quality-conscious person with top-notch teamwork and customer skills. I have excellent communication skills, very good at multitasking, and I adhere to deadlines.";
 
 // Render personal information into HTML
 document.addEventListener("DOMContentLoaded", function () {
   const bioParagraph = document.querySelector(".bio-text p");
-  const boldHello = "<b>Hello</b>"; // Bold "Hello"
-  const updatedBio = bio.replace("Hello", boldHello); // Replaced "Hello" with bold version
-
-  document.querySelector(".bio-text h2").textContent = `${name} ${surname}`;
-  bioParagraph.innerHTML = updatedBio; // Use innerHTML to render HTML content
+  bioParagraph.innerHTML = bio; // Use innerHTML to render HTML content
   renderProjects();
+  renderContactInfo();
 });
